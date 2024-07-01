604045dab487a3c6cdc68cd17dbfd0dcd5ce2f9c
MayankArora-SDET <mayank@sdettech.com>
2024-07-01 19:26:15 +0530

    API fixes

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,7 +7,8 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {"src/test/resources/features/"},
-glue = {"stepDefination"} , tags = "@API")
+glue = {"stepDefination"} , tags = "@API",  plugin = {"pretty", "json:target/cucumber-report/cucumber.json"},
+publish = true)
 public class Runner{
 
 }
